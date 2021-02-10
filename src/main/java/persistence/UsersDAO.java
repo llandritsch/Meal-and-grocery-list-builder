73df2e605b61ceb064c88f5b4d42ad13dae59121
Lisa Andritsch <lisa3718@gmail.com>
2021-02-10 15:36:23 -0600

    added UsersDAO class with getAllUsers method

---
+++ src/main/java/persistence/UsersDAO.java
@@ -0,0 +1,32 @@
+package persistence;
+
+import entity.Users;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class UsersDAO {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+    /**
+     * Creates the proper sql statement for search and sends the sql statement to be executed
+     * @return users
+     */
+    public List<Users> getAllUsers() {
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Users> query = builder.createQuery(Users.class);
+        Root<Users> root = query.from(Users.class);
+        List<Users> users = session.createQuery(query).getResultList();
+        session.close();
+        return users;
+    }
+}
+
