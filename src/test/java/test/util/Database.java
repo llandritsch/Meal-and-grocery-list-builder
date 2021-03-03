53745ddbe3040bc43fcb48f17cefd833ceefcf4f
Lisa Andritsch <lisa3718@gmail.com>
2021-03-02 20:15:56 -0600

    fixed system.out's

--- src/test/java/test/util/Database.java
+++ src/test/java/test/util/Database.java
@@ -41,10 +41,10 @@ public class Database {
         try {
             properties.load (this.getClass().getResourceAsStream("/database.properties"));
         } catch (IOException ioe) {
-            System.out.println("Database.loadProperties()...Cannot load the properties file");
+            logger.info("Database.loadProperties()...Cannot load the properties file");
             ioe.printStackTrace();
         } catch (Exception e) {
-            System.out.println("Database.loadProperties()..." + e);
+            logger.info("Database.loadProperties()..." + e);
             e.printStackTrace();
         }
 
@@ -78,7 +78,7 @@ public class Database {
             try {
                 connection.close();
             } catch (SQLException e) {
-                System.out.println("Cannot close connection" + e);
+                logger.info("Cannot close connection" + e);
             }
         }
 
