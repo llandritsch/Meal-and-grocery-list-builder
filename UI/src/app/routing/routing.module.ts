5f5975cd524e7d4558e9ba6402a303b40227aa13
Lisa Andritsch <lisa3718@gmail.com>
2021-03-14 18:31:01 -0500

    added router module

---
+++ UI/src/app/routing/routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent} from "../index/index.component";
+
+const routes: Routes = [
+  { path: 'index', component: IndexComponent},
+  { path: '',redirectTo: '/home', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class RoutingModule { }
