6530574fcbe6c3795c99ea9828c7d17c9cb48196
Lisa Andritsch <lisa3718@gmail.com>
2021-04-20 19:36:00 -0500

    added get recipe by name and post method to recipe service

--- src/main/java/controller/RecipeService.java
+++ src/main/java/controller/RecipeService.java
@@ -4,10 +4,7 @@ import entity.Recipes;
 import entity.Users;
 import persistence.RecipesDAO;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -28,13 +25,40 @@ public class RecipeService {
     }
 
     @GET
-    @Path("/goals/{recipeid}")
+    @Path("/recipes/{recipeid}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getUserById(
+    public Response getRecipeById(
             @PathParam("recipeid") int recipeid
     ) {
         Recipes recipes = new RecipesDAO().getRecipeById(recipeid);
         GenericEntity<Recipes> myEntity = new GenericEntity<Recipes>(recipes) {};
         return Response.status(200).entity(myEntity).build();
     }
+
+    @GET
+    @Path("/recipes/{recipeName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRecipeByName(
+            @PathParam("recipeName") String recipeName
+    ) {
+        List<Recipes> recipes = new RecipesDAO().getRecipeByName(recipeName);
+        GenericEntity<List<Recipes>> myEntity = new GenericEntity<List<Recipes>>(recipes) {};
+        return Response.status(200).entity(myEntity).build();
+    }
+
+    @POST
+    @Path("/recipes")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createRecipe(Recipes recipe) {
+        int id = dao.createRecipe(recipe);
+        if(id != 0) {
+            recipe.setRecipe_id(id);
+            GenericEntity<Recipes> myEntity = new GenericEntity<>(recipe) {};
+            return Response.status(200).entity(myEntity).build();
+        } else {
+            return Response.status(500).build();
+        }
+    }
+
 }
