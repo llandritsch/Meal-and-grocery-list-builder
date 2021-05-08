18aabd2d193fd440152960fb4000cd54bd14e1ac
Lisa Andritsch <lisa3718@gmail.com>
2021-05-08 07:46:31 -0500

    Add comment explaining what StaticFileService does

--- src/main/java/controller/StaticFileService.java
+++ src/main/java/controller/StaticFileService.java
@@ -22,7 +22,12 @@ public class StaticFileService {
         InputStream indexHtml = context.getResourceAsStream("index.html");
 
         return Objects.isNull(resource)
+                // If the resource does not exist, then respond with the index.html page. This should
+                // load the user into the Angular application and from there, routing will take over.
                 ? Response.ok().entity(indexHtml).build()
+                // If the resource DOES exist, then it's likely the index.html page trying to load
+                // the JS/CSS files. Serve those up instead, so that the page can load the Angular
+                // code.
                 : Response.ok().entity(resource).build();
     }
 
